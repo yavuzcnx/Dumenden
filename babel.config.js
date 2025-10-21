fc225ab171d1b9d68315b75cb7710170052afbcb
yavuzcnx <yavuzcnxss@gmail.com>
2025-10-21 07:36:10 +0300

    fix: module-resolver to deps, valid package.json, correct assets paths

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['module-resolver', {
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+        alias: {
+          '@': './',
+          '@app': './app',
+          '~': './src'
+        }
+      }],
+      'react-native-reanimated/plugin'
+    ],
+  };
+};
